6fcdcb7d259a4d4193a0197e23a8e4c6b4464b67
julialiaw <juliawux01@gmail.com>
2026-06-29 12:01:57 -0400

    fixed nav bar for mobile screens

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var header = document.getElementById("header");
+var header = document.getElementById("headerOutline");
 function showMenu() {
-    header.style.right = "0";
+    header.style.right = "0px";
 }
 function hideMenu() {
     header.style.right = "-200px";
